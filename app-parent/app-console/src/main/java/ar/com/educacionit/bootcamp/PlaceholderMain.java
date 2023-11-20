de7b9d3e815bdf7060df4b45e4527dc8f5d3518f
Sheila Diz <dizsheila07@gmail.com>
2023-11-20 18:57:58 -0300

    Revert "feat: agregar conexion a API placeholder"
    This reverts commit ab4c4cccf5e6df542f73b021309dbd48786984d2.

--- app-parent/app-console/src/main/java/ar/com/educacionit/bootcamp/PlaceholderMain.java
+++ app-parent/app-console/src/main/java/ar/com/educacionit/bootcamp/PlaceholderMain.java
@@ -1,6 +1,5 @@
 package ar.com.educacionit.bootcamp;
 
-import ar.com.educacionit.bootcamp.connectors.placeholder.PlaceholderApiService;
 import ar.com.educacionit.bootcamp.dto.Placeholder;
 import ar.com.educacionit.bootcamp.service.ServiceLocator;
 import ar.com.educacionit.bootcamp.service.placeholder.PlaceholderService;
@@ -12,21 +11,7 @@ public class PlaceholderMain {
 		
 		Placeholder phDB = serviceDB.buscarPorId(1);
 		
-		if(phDB!=null) {
-			System.out.println("Se encontro en db" + phDB);			
-		}else {
-			PlaceholderApiService serviceAPI = (PlaceholderApiService) ServiceLocator.getService(PlaceholderApiService.class);
-			Placeholder phAPI = serviceAPI.buscarPorId(1);
-			
-			serviceDB.guardar(phAPI);
-
-			phDB = serviceDB.buscarPorId(1);
-			System.out.println("Se agrego a la db" + phDB);			
-		}
-		
-		
-		
-		
+		System.out.println(phDB);
 	}
 
 }
--- app-parent/app-rest-api-connector/src/main/java/ar/com/educacionit/bootcamp/connectors/placeholder/PlaceholderApiService.java
+++
@@ -1,8 +0,0 @@
-package ar.com.educacionit.bootcamp.connectors.placeholder;
-
-import ar.com.educacionit.bootcamp.dto.Placeholder;
-import ar.com.educacionit.bootcamp.service.EntidadBaseService;
-
-public interface PlaceholderApiService extends EntidadBaseService<Placeholder> {
-
-}
