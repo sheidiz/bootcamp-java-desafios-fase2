ab4c4cccf5e6df542f73b021309dbd48786984d2
Sheila Diz <dizsheila07@gmail.com>
2023-11-19 23:18:54 -0300

    feat: agregar conexion a API placeholder

---
+++ app-parent/app-rest-api-connector/src/main/java/ar/com/educacionit/bootcamp/connectors/placeholder/PlaceholderApiService.java
@@ -0,0 +1,8 @@
+package ar.com.educacionit.bootcamp.connectors.placeholder;
+
+import ar.com.educacionit.bootcamp.dto.Placeholder;
+import ar.com.educacionit.bootcamp.service.EntidadBaseService;
+
+public interface PlaceholderApiService extends EntidadBaseService<Placeholder> {
+
+}
