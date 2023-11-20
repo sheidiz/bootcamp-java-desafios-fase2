b16fd05ec61ced062f6a6b46b521947b2f73040f
Sheila Diz <dizsheila07@gmail.com>
2023-11-20 19:17:06 -0300

    feat: conectar tomcat

--- app-parent/app-console/src/main/java/ar/com/educacionit/bootcamp/CovidMain.java
+++ app-parent/app-console/src/main/java/ar/com/educacionit/bootcamp/CovidMain.java
@@ -1,29 +1,23 @@
 package ar.com.educacionit.bootcamp;
 
-import ar.com.educacionit.bootcamp.connectors.covid.CovidApiService;
-import ar.com.educacionit.bootcamp.dto.Covid;
-import ar.com.educacionit.bootcamp.service.ServiceLocator;
-import ar.com.educacionit.bootcamp.service.covid.CovidService;
-
 public class CovidMain {
 	public static void main(String[] args) {
-		CovidService serviceDB = (CovidService) ServiceLocator.getService(CovidService.class);
-		
-		System.out.println("---Buscar por fecha 15/02/2020 en DB---");
-		String newDate = "2020-02-15".replaceAll("-", "");
-		Covid covidDB = serviceDB.buscarPorCampo("date", newDate);
-
-		if(covidDB !=null) {
-			System.out.println("Se encontro en db" + covidDB);
-		}else {
-			CovidApiService serviceAPI = (CovidApiService) ServiceLocator.getService(CovidApiService.class);
-			Covid covidAPI = serviceAPI.buscarPorCampo("date", newDate);
-			
-			serviceDB.guardar(covidAPI);
-			covidDB = serviceDB.buscarPorCampo("date", newDate);
-			System.out.println("Se guardo en db" + covidDB);
-			
-		}
-		
+//		CovidService serviceDB = (CovidService) ServiceLocator.getService(CovidService.class);
+//		
+//		System.out.println("---Buscar por fecha 15/02/2020 en DB---");
+//		String newDate = "2020-02-15".replaceAll("-", "");
+//		Covid covidDB = serviceDB.buscarPorCampo("date", newDate);
+//
+//		if(covidDB !=null) {
+//			System.out.println("Se encontro en db" + covidDB);
+//		}else {
+//			CovidApiService serviceAPI = (CovidApiService) ServiceLocator.getService(CovidApiService.class);
+//			Covid covidAPI = serviceAPI.buscarPorCampo("date", newDate);
+//			
+//			serviceDB.guardar(covidAPI);
+//			covidDB = serviceDB.buscarPorCampo("date", newDate);
+//			System.out.println("Se guardo en db" + covidDB);
+//			}
 	}
+		
 }
--- app-parent/app-console/src/main/java/ar/com/educacionit/bootcamp/PlaceholderMain.java
+++ app-parent/app-console/src/main/java/ar/com/educacionit/bootcamp/PlaceholderMain.java
@@ -1,28 +1,28 @@
 package ar.com.educacionit.bootcamp;
 
-import ar.com.educacionit.bootcamp.connectors.placeholder.PlaceholderApiService;
-import ar.com.educacionit.bootcamp.dto.Placeholder;
-import ar.com.educacionit.bootcamp.service.ServiceLocator;
-import ar.com.educacionit.bootcamp.service.placeholder.PlaceholderService;
+//import ar.com.educacionit.bootcamp.connectors.placeholder.PlaceholderApiService;
+//import ar.com.educacionit.bootcamp.dto.Placeholder;
+//import ar.com.educacionit.bootcamp.service.ServiceLocator;
+//import ar.com.educacionit.bootcamp.service.placeholder.PlaceholderService;
 
 public class PlaceholderMain {
 
 	public static void main(String[] args) {
-		PlaceholderService serviceDB = (PlaceholderService) ServiceLocator.getService(PlaceholderService.class);
-		
-		Placeholder phDB = serviceDB.buscarPorId(1);
-		
-		if(phDB!=null) {
-			System.out.println("Se encontro en db" + phDB);			
-		}else {
-			PlaceholderApiService serviceAPI = (PlaceholderApiService) ServiceLocator.getService(PlaceholderApiService.class);
-			Placeholder phAPI = serviceAPI.buscarPorId(1);
-			
-			serviceDB.guardar(phAPI);
-
-			phDB = serviceDB.buscarPorId(1);
-			System.out.println("Se agrego a la db" + phDB);			
-		}
+//		PlaceholderService serviceDB = (PlaceholderService) ServiceLocator.getService(PlaceholderService.class);
+//		
+//		Placeholder phDB = serviceDB.buscarPorId(1);
+//		
+//		if(phDB!=null) {
+//			System.out.println("Se encontro en db" + phDB);			
+//		}else {
+//			PlaceholderApiService serviceAPI = (PlaceholderApiService) ServiceLocator.getService(PlaceholderApiService.class);
+//			Placeholder phAPI = serviceAPI.buscarPorId(1);
+//			
+//			serviceDB.guardar(phAPI);
+//
+//			phDB = serviceDB.buscarPorId(1);
+//			System.out.println("Se agrego a la db" + phDB);			
+//		}
 		
 		
 		
