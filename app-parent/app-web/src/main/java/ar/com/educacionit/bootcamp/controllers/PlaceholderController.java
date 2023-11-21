832c00a4d5dddeaffb308e50bd855e0d355a2fb0
Sheila Diz <dizsheila07@gmail.com>
2023-11-21 16:33:39 -0300

    feat: agregar boton placeholder funcional 💪

---
+++ app-parent/app-web/src/main/java/ar/com/educacionit/bootcamp/controllers/PlaceholderController.java
@@ -0,0 +1,51 @@
+package ar.com.educacionit.bootcamp.controllers;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ar.com.educacionit.bootcamp.connectors.placeholder.PlaceholderApiService;
+import ar.com.educacionit.bootcamp.dto.Placeholder;
+import ar.com.educacionit.bootcamp.service.ServiceLocator;
+import ar.com.educacionit.bootcamp.service.placeholder.PlaceholderService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/api/placeholder")
+public class PlaceholderController extends HttpServlet {
+
+	private static final long serialVersionUID = 8779896911337927637L;
+	private PlaceholderService serviceDB = (PlaceholderService) ServiceLocator.getService(PlaceholderService.class);
+	private PlaceholderApiService serviceAPI = (PlaceholderApiService) ServiceLocator
+			.getService(PlaceholderApiService.class);
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.addHeader("Access-Control-Allow-Origin", "http://localhost:5500");
+		
+		Integer id = Integer.parseInt(req.getParameter("id"));
+		Placeholder post = serviceDB.buscarPorId(id);
+		
+		// si no lo encontro lo busca en la api
+		if (post == null) {
+			Placeholder postAPI = serviceAPI.buscarPorId(id);
+			if(postAPI!=null) {
+				serviceDB.guardar(postAPI);
+				post= serviceDB.buscarPorId(id);
+			}
+		}
+		this.enviarJson(resp, post);
+	}
+	private void enviarJson (HttpServletResponse resp, Placeholder post) throws IOException {
+		if(post!= null) {
+			resp.setContentType("application/json");
+			ObjectMapper objectMapper = new ObjectMapper();
+			String jsonPost = objectMapper.writeValueAsString(post);
+			resp.getWriter().print(jsonPost);
+		}
+	}
+}
--- app-parent/app-web/src/main/webapp/getData.js
+++ app-parent/app-web/src/main/webapp/getData.js
@@ -4,7 +4,7 @@ const getPokemon = () => {
   var idPokemon = document.getElementById("pokemonId").value;
   var intIdPokemon = parseInt(idPokemon);
 
-  if(intIdPokemon >=1  && intIdPokemon <=1017){
+  if (intIdPokemon >= 1 && intIdPokemon <= 1017) {
     var url = 'http://localhost:8080/app-web/api/pokemon?id=' + idPokemon;
     fetch(url)
       .then(
@@ -14,21 +14,19 @@ const getPokemon = () => {
           content.innerHTML = `
           <p>ID: ${data.id}</p>
           <p>Nombre: ${data.name}</p>
-          <p>Altura: ${data.height/10}m</p>
-          <p>Peso: ${data.weight/10}kg</p>
-      `;
+          <p>Altura: ${data.height / 10}m</p>
+          <p>Peso: ${data.weight / 10}kg</p>`;
         }
       ).catch(
         error => { console.log("Error al buscar el Pokemon", error); }
       );
   }
-
 }
 
 const getCovidUSData = () => {
   var dateCovid = document.getElementById("covidDate").value;
   console.log(dateCovid);
-  var strDateCovid = dateCovid.replaceAll("-","");
+  var strDateCovid = dateCovid.replaceAll("-", "");
   console.log(strDateCovid);
 
   var url = 'http://localhost:8080/app-web/api/covid?date=' + strDateCovid;
@@ -41,10 +39,32 @@ const getCovidUSData = () => {
         <p>Fecha: ${data.date}</p>
         <p>Positivos: ${data.positive}</p>
         <p>Negativos: ${data.negative}</p>
-        <p>Pendientes: ${data.pending}</p>
-    `;
+        <p>Pendientes: ${data.pending}</p>`;
       }
     ).catch(
       error => { console.log("Error al buscar el Pokemon", error); }
     );
+}
+
+const getPost = () => {
+  var idPost = document.getElementById("postId").value;
+  var intIdPost = parseInt(idPost);
+
+  if (intIdPost >= 1 && intIdPost <= 100) {
+    var url = 'http://localhost:8080/app-web/api/placeholder?id=' + intIdPost;
+    fetch(url)
+      .then(
+        response => response.json()
+      ).then(
+        data => {
+          content.innerHTML = `
+          <p>ID: ${data.id}</p>
+          <p>ID de Usuario: ${data.userId}</p>
+          <p>Titulo: ${data.title}</p>
+          <p>Cuerpo: ${data.body}</p>`;
+        }
+      ).catch(
+        error => { console.log("Error al buscar el Post", error); }
+      );
+  }
 }
