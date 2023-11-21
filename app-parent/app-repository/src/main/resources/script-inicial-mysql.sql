10e1204a154a2d1d1e034d7a94e60c8b2b2784a7
Sheila Diz <dizsheila07@gmail.com>
2023-11-21 16:05:26 -0300

    feat: editar script sql

--- app-parent/app-repository/src/main/resources/script-inicial-mysql.sql
+++ app-parent/app-repository/src/main/resources/script-inicial-mysql.sql
@@ -1,44 +1,40 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.1.0 - MySQL Community Server - GPL
--- Server OS:                    Linux
--- HeidiSQL Version:             12.5.0.6677
--- --------------------------------------------------------
-
--- Dumping database structure for bootcamp-java-desafio5
 CREATE DATABASE IF NOT EXISTS `bootcamp-java-desafio5`
 USE `bootcamp-java-desafio5`;
 
--- Dumping structure for table bootcamp-java-desafio5.POKEMON
+#CREAR TABLAS
 CREATE TABLE IF NOT EXISTS `POKEMON` (
-  `id` int NOT NULL,
-  `name` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL DEFAULT '',
-  `baseExperience` int NOT NULL DEFAULT (0),
-  `height` int NOT NULL DEFAULT (0),
-  `weight` int NOT NULL DEFAULT (0),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  	id INT NOT NULL,
+  	name VARCHAR(50) NOT NULL DEFAULT '',
+  	baseExperience INT NOT NULL DEFAULT (0),
+  	height INT NOT NULL DEFAULT (0),
+  	weight INT NOT NULL DEFAULT (0),
+  	PRIMARY KEY (id)
+)COLLATE=utf8mb4_spanish_ci;
 
-CREATE TABLE `COVID` (
-	`date` VARCHAR(8) NOT NULL DEFAULT '' COLLATE 'utf8mb4_spanish_ci',
-	`states` INT(10) NOT NULL,
-	`positive` INT(10) NOT NULL,
-	`negative` INT(10) NOT NULL,
-	`pending` INT(10) NOT NULL,
-	`hash` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_spanish_ci',
-	PRIMARY KEY (`date`) USING BTREE
-)
-COLLATE='utf8mb4_spanish_ci'
-ENGINE=InnoDB
-;
+CREATE TABLE COVID (
+	date VARCHAR(8) NOT NULL DEFAULT '',
+	states INT(10) NOT NULL,
+	positive INT(10) NOT NULL,
+	negative INT(10) NOT NULL,
+	pending INT(10) NOT NULL,
+	hash VARCHAR(50) NOT NULL DEFAULT '',
+	PRIMARY KEY (date) USING BTREE
+)COLLATE='utf8mb4_spanish_ci';
 
-CREATE TABLE `PLACEHOLDER` (
-	`id` INT(10) NOT NULL,
-	`userId` INT(10) NOT NULL,
-	`title` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_spanish_ci',
-	`body` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_spanish_ci',
-	PRIMARY KEY (`id`) USING BTREE
-)
-COLLATE='utf8mb4_spanish_ci'
-ENGINE=InnoDB
-;
+CREATE TABLE PLACEHOLDER (
+	id INT(10) NOT NULL,
+	userId INT(10) NOT NULL,
+	title VARCHAR(200) NOT NULL DEFAULT '',
+	body VARCHAR(300) NOT NULL DEFAULT '',
+	PRIMARY KEY (id) USING BTREE
+)COLLATE='utf8mb4_spanish_ci';
+
+#INSERTAR CONTENIDO
+INSERT INTO POKEMON (id, name, baseExperience, height, weight) VALUES (1, 'bulbasaur', 64, 7, 69)
+
+INSERT INTO POKEMON (id, name, baseExperience, height, weight) VALUES (2, 'ivysaur', 142, 10, 130)
+
+INSERT INTO COVID (date, states, positive, negative, pending, hash) VALUES ('20200101', 1, 100, 100, 100, 'hash3921839028')
+
+INSERT INTO PLACEHOLDER (id, userId, title, body) 
+VALUES (1, 1, 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto')
